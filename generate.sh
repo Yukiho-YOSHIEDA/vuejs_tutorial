37887d77df09b767bcf188197be10137712bdbf8
yukiho <is0436er@ed.ritsumei.ac.jp>
2019-02-21 02:47:58 +0900

    [add] shell script generate template files

---
+++ generate.sh
@@ -0,0 +1,22 @@
+# 引数確認
+if [ $# -ne 1 ]; then
+  echo "フォルダ名を指定してください"
+  exit 1
+fi
+
+# フォルダの名前を取得
+FOLDER_NAME=$1
+
+# shell実行ファイルのディレクトに移動
+cd $(dirname $0)
+
+# フォルダがあれば異常終了
+if [ -e $FOLDER_NAME ]; then
+  echo "そのフォルダはすでに存在します"
+  exit 1
+fi
+
+mkdir $FOLDER_NAME
+cp template/index.html $FOLDER_NAME/index.html
+cp template/index.js $FOLDER_NAME/index.js
+
